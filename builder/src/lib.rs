5d6fc287da5d71e50b5b1e20af5922645ca4b7f8
Till Zickmantel <till.zickmantel@gmail.com>
2021-07-14 13:16:18 +0200

    ADD: Procmarco dependencies and AST output

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,12 @@
+extern crate proc_macro;
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    eprintln!("Here is the AST: {:#?}", ast);
+    let _ = ast;
     TokenStream::new()
 }
--- builder/tests/progress.rs
+++ builder/tests/progress.rs
@@ -2,7 +2,7 @@
 fn tests() {
     let t = trybuild::TestCases::new();
     t.pass("tests/01-parse.rs");
-    //t.pass("tests/03-create-builder.rs");
+    t.pass("tests/02-create-builder.rs");
     //t.pass("tests/04-call-setters.rs");
     //t.pass("tests/07-call-build.rs");
     //t.pass("tests/06-method-chaining.rs");
