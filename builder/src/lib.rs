262523e2f4ef1e57e2ce2e2b226837acf5a1da05
Till Zickmantel <till.zickmantel@gmail.com>
2021-07-14 14:42:09 +0200

    ADD: Builder parameters

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -5,16 +5,50 @@ use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
+
     let ast = parse_macro_input!(input as DeriveInput);
     let name = &ast.ident;
     let bname = format!("{}Builder", name);
     let bident = Ident::new(&bname, name.span());
+
     quote!(
-        struct #bident;
+
+        pub struct #bident{
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>
+        }
+
         impl #name{
-            fn builder() -> #bident {
-                #bident{}
+            pub fn builder() -> #bident {
+                #bident{
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
             }
         }
+
+        impl #bident {
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+        }
+
     ).into()
 }
