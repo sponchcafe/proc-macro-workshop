69a92f5d3a5454e5893b801eaf2c194c7f6e1116
Till Zickmantel <till.zickmantel@gmail.com>
2021-07-14 13:18:58 +0200

    ADD: Empty builder implementation via macro

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -5,8 +5,12 @@ use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(input as DeriveInput);
-    eprintln!("Here is the AST: {:#?}", ast);
-    let _ = ast;
-    TokenStream::new()
+    let _ast = parse_macro_input!(input as DeriveInput);
+    quote!(
+        impl Command {
+            pub fn builder() {}
+        }
+    ).into()
+
+    
 }
