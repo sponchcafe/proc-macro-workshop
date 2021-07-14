889b359ba138c8bd5455fa445bc79e759c0d5ca7
Till Zickmantel <till.zickmantel@gmail.com>
2021-07-14 13:52:37 +0200

    ADD: Create builder struct in macro

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,16 +1,20 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ast = parse_macro_input!(input as DeriveInput);
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = Ident::new(&bname, name.span());
     quote!(
-        impl Command {
-            pub fn builder() {}
+        struct #bident;
+        impl #name{
+            fn builder() -> #bident {
+                #bident{}
+            }
         }
     ).into()
-
-    
 }
